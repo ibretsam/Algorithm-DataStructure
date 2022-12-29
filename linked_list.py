b2e68027c0126a496d21a0c8a2e6bcfd0cc8f2f1
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2022-12-29 13:43:34 +0700

    added node_at_index method

--- linked_list.py
+++ linked_list.py
@@ -125,6 +125,20 @@ class LinkedList:
                             
             prev_node.next_node = current.next_node
         return self
+    
+    def node_at_index(self, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+            
+            while position < index:
+                current = current.next_node
+                position += 1
+                
+            return current
+            
         
     def __repr__(self) -> str:
         """
